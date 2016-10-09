1f4b74eaae5bb78bfcde6dfd7bf459ef8bf3ea67
Michael Glatzmaier <michael.glatzmaier@gmail.com>
2016-10-09 09:31:34 -0500

    refactoring PBIL code.

--- algorithm/mlearning/PBIL.h
+++ algorithm/mlearning/PBIL.h
@@ -9,7 +9,7 @@ Population based incremental learning --
 
 #include "../../system/types.h"
 
-typedef float (*cost_func)(void*);
+typedef float (*cost_func)(int*, void*);
 
 class PBIL
 {
@@ -24,23 +24,24 @@ public:
 	~PBIL() {}
 
 	void optimize(cost_func cf, void * params, float learn_rate, float neg_learn_rate, float mutation_probabilty, float mutation_shift, uint iterations);
-	int * load_genes(float * probabilities);
+	int ** create_genes(float * probabilities);
 	void update_probabilities(float * probabilities, int * min_gene, int * max_gene, float learn_rate, float neg_learn_rate);
 	void mutate(float * probabilities, float mutation_probabilty, float mutation_shift);
 };
 
 
 
-int * PBIL::load_genes(float * probabilities)
+int ** PBIL::create_genes(float * probabilities)
 {
 	std::uniform_real_distribution<double> dist(0, 1);
-	int * genes = new int[population * num_bits];
-	for (int i = 0, idx = 0; i < population; ++i)
+	int ** genes = new int*[population]; 
+	for (int k = 0; k < population;++k)  genes[k] = new int[num_bits];
+	for (int p = 0; p < population; ++p)
 	{
-		for (int j = 0; j < num_bits; ++j, ++idx)
+		for (int j = 0; j < num_bits; ++j)
 		{
-			if (dist(rng) < probabilities[j]) genes[idx] = 1;
-			else genes[idx] = 0;
+			if (dist(rng) < probabilities[j]) genes[p][j] = 1;
+			else genes[p][j] = 0;
 		}
 	}
 	return genes;
@@ -79,19 +80,19 @@ void PBIL::optimize(cost_func cf, void * params, float learn_rate, float neg_lea
 	if (population <= 0) population = 512;
 	float * probabilities = new float[num_bits];	
 	for (int i = 0; i < num_bits; ++i) probabilities[i] = 0.5f;
-	int * best_gene = new int[num_bits];
+	int * best_gene = 0;
 
 	for (int i = 0; i < iterations; i++)
 	{
 		// initialize population
-		int * genes = load_genes(probabilities);
+		int ** genes = create_genes(probabilities);
 
 		// compute costs
 		float * costs = new float[population];
-		for (int j = 0; j < population; ++j) costs[j] = cf(params);
+		for (int j = 0; j < population; ++j) costs[j] = cf(genes[j], params);
 
 		// get max/min bounds
-		int * min_gene = new int[num_bits]; int * max_gene = new int[num_bits]; 
+		int * min_gene = 0; int * max_gene = 0; 
 		double min_cost = 1e10; double max_cost = -1e10;
 		for (int j = 0; j < population; ++j)
 		{
@@ -99,28 +100,27 @@ void PBIL::optimize(cost_func cf, void * params, float learn_rate, float neg_lea
 			if (min_cost > c)
 			{
 				min_cost = c;
-				for (int k = population*num_bits, idx = 0; k < (population + 1)*num_bits; ++k, ++idx) min_gene[idx] = genes[k];
+				min_gene = genes[j];
 			}
 			if (max_cost < c)
 			{
 				max_cost = c;
-				for (int k = population*num_bits, idx = 0; k < (population + 1)*num_bits; ++k, ++idx) max_gene[idx] = genes[k];
+				max_gene = genes[j];
 			}
 		}
 
 		if (best_cost > min_cost)
 		{
 			best_cost = min_cost;
-			for (int k = population*num_bits, idx = 0; k < (population + 1)*num_bits; ++k, ++idx) best_gene[idx] = genes[k];
+			best_gene = min_gene;
 		}
 		
 		update_probabilities(probabilities, min_gene, max_gene, learn_rate, neg_learn_rate);
 		mutate(probabilities, mutation_probabilty, mutation_shift);
 
-		// free memory
-		if (min_gene) { delete min_gene; min_gene = 0; }
-		if (max_gene) { delete max_gene; max_gene = 0; }
-		if (costs) { delete costs; costs = 0; }
+		// free memory		
+		if (genes) { for (int j = 0; j < population; ++j) { delete[] genes[j]; genes[j] = 0; } delete [] genes; genes = 0; }
+		if (costs) { delete [] costs; costs = 0; }
 	}
 }
 
